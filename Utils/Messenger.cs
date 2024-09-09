33d160b508e01d6eeff09de6a808c6deaf34b699
Choi <escaco95@naver.com>
2024-09-09 13:37:44 +0900

    add messenger.cs
    - 컨트롤 간 메신저 통신을 가능케 하는 모듈 개발

---
+++ Utils/Messenger.cs
@@ -0,0 +1,44 @@
+﻿namespace vJassMainJBlueprint.Utils
+{
+    public static class Messenger
+    {
+        // Dictionary to store event handlers for different message types
+        private static readonly Dictionary<Type, List<Action<object>>> _subscriptions = [];
+
+        // Subscribe to a specific message type
+        public static void Subscribe<TMessage>(Action<TMessage> action)
+        {
+            var messageType = typeof(TMessage);
+            if (!_subscriptions.TryGetValue(messageType, out List<Action<object>>? value))
+            {
+                value = [];
+                _subscriptions[messageType] = value;
+            }
+
+            value.Add(msg => action((TMessage)msg));
+        }
+
+        // Unsubscribe from a specific message type
+        public static void Unsubscribe<TMessage>(Action<TMessage> action)
+        {
+            var messageType = typeof(TMessage);
+            if (_subscriptions.TryGetValue(messageType, out List<Action<object>>? value))
+            {
+                value.RemoveAll(a => a.Equals(action));
+            }
+        }
+
+        // Send a message to all subscribers of that message type
+        public static void Send<TMessage>(TMessage message) where TMessage : notnull
+        {
+            var messageType = typeof(TMessage);
+            if (_subscriptions.TryGetValue(messageType, out List<Action<object>>? value))
+            {
+                foreach (var action in value)
+                {
+                    action(message);
+                }
+            }
+        }
+    }
+}
