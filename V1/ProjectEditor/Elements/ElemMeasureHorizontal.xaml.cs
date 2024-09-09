99742e08ee2210c7744791ecadf3c511705122f4
Choi <escaco95@naver.com>
2024-09-09 21:12:57 +0900

    fix ProjectEditWorkspace
    - split elements

---
+++ V1/ProjectEditor/Elements/ElemMeasureHorizontal.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace vJassMainJBlueprint.V1.ProjectEditor.Elements
+{
+    /// <summary>
+    /// ElemMeasureHorizontal.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ElemMeasureHorizontal : UserControl
+    {
+        public ElemMeasureHorizontal()
+        {
+            InitializeComponent();
+        }
+
+        // DependencyProperty for Text
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(ElemMeasureHorizontal), new PropertyMetadata("2000"));
+
+        // CLR wrapper for Text
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
---
+++ V1/ProjectEditor/Elements/ElemMeasureVertical.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace vJassMainJBlueprint.V1.ProjectEditor.Elements
+{
+    /// <summary>
+    /// ElemMeasureVertical.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ElemMeasureVertical : UserControl
+    {
+        public ElemMeasureVertical()
+        {
+            InitializeComponent();
+        }
+
+        // DependencyProperty for Text
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(ElemMeasureVertical), new PropertyMetadata("2000"));
+
+        // CLR wrapper for Text
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
