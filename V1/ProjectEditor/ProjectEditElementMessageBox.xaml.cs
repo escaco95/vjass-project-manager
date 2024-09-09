eef4b9ba850eee795658131350929e8ab9a91a26
Choi <escaco95@naver.com>
2024-09-09 13:43:15 +0900

    fix MessageBox
    - in-app 안내 메시지가 메신저 통신을 통해 자신을 표시할 수 있도록 수정
    - 코드 정리

--- V1/ProjectEditor/ProjectEditElementMessageBox.xaml.cs
+++ V1/ProjectEditor/ProjectEditElementMessageBox.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Threading;
+using vJassMainJBlueprint.Utils;
 
 namespace vJassMainJBlueprint.V1.ProjectEditor
 {
@@ -10,70 +11,101 @@ namespace vJassMainJBlueprint.V1.ProjectEditor
     /// </summary>
     public partial class ProjectEditElementMessageBox : UserControl
     {
-        private readonly TimeSpan fadeDuration = TimeSpan.FromSeconds(0.5); // 페이드 아웃 지속 시간
-        private readonly TimeSpan visibleDuration = TimeSpan.FromSeconds(0.5); // 메시지가 보이는 시간
-        private readonly DispatcherTimer collapseTimer = new ();
+        private static readonly TimeSpan DefaultFadeDuration = TimeSpan.FromSeconds(0.5);
+        private static readonly TimeSpan DefaultVisibleDuration = TimeSpan.FromSeconds(0.5);
+        private readonly DispatcherTimer _collapseTimer = new();
 
         public ProjectEditElementMessageBox()
         {
             InitializeComponent();
 
-            collapseTimer.Interval = TimeSpan.FromSeconds(1);
-            collapseTimer.Tick += (sender, e) =>
+            _collapseTimer.Tick += (sender, e) =>
             {
-                collapseTimer.Stop();
+                _collapseTimer.Stop();
                 Visibility = System.Windows.Visibility.Collapsed;
             };
-        }
-
-        // 메시지 표시 후 페이드 아웃 애니메이션을 실행하는 메서드
-        private void ShowMessage(string message, Brush backgroundColor)
-        {
-            MessageTextBlock.Text = message;
-            BlockBackdrop.Background = backgroundColor;
-
-            // 이전 애니메이션이 있으면 중지
-            BlockRoot.BeginAnimation(OpacityProperty, null);
-
-            // 메시지 표시
-            Visibility = System.Windows.Visibility.Visible;
-            BlockRoot.Opacity = 1;
-
-            // 페이드 아웃 애니메이션 초기화 및 설정
-            DoubleAnimation fadeOutAnimation = new(0, fadeDuration)
-            {
-                BeginTime = visibleDuration
-            };
-
-            // 페이드 아웃 애니메이션 완료 후 메시지 숨김
-            collapseTimer.Stop();
-            collapseTimer.Start();
-
-            // 페이드 아웃 애니메이션 (잠시 후 실행)
-            BlockRoot.BeginAnimation(OpacityProperty, fadeOutAnimation);
-        }
-
-        // Info 메시지 표시
-        public void Info(string message)
-        {
-            ShowMessage(message, Brushes.Blue);
-        }
-
-        // Warn 메시지 표시
-        public void Warn(string message)
-        {
-            ShowMessage(message, Brushes.Orange);
-        }
-
-        // Error 메시지 표시
-        public void Error(string message)
-        {
-            ShowMessage(message, Brushes.Red);
         }
 
         private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             BlockRoot.Opacity = 0;
+            Messenger.Subscribe<ShowActionMessage>((message) => { Dispatcher.Invoke(() => ShowMessage(message)); });
+        }
+
+        public void Info(string message)
+        {
+            ShowMessage(new ShowActionMessage(message, ShowActionMessage.LevelType.Info));
+        }
+
+        public void Warn(string message)
+        {
+            ShowMessage(new ShowActionMessage(message, ShowActionMessage.LevelType.Warn));
+        }
+
+        public void Error(string message)
+        {
+            ShowMessage(new ShowActionMessage(message, ShowActionMessage.LevelType.Error));
+        }
+
+        private void ShowMessage(ShowActionMessage message)
+        {
+            MessageTextBlock.Text = message.Message;
+            BlockBackdrop.Background = GetMessageLevelBrush(message.Level);
+
+            BlockRoot.BeginAnimation(OpacityProperty, null);
+
+            Visibility = System.Windows.Visibility.Visible;
+            BlockRoot.Opacity = 1;
+
+            DoubleAnimation fadeOutAnimation = new(0, message.FadeDuration) { BeginTime = message.VisibleDuration };
+
+            _collapseTimer.Stop();
+            _collapseTimer.Interval = message.FadeDuration + message.VisibleDuration;
+            _collapseTimer.Start();
+
+            BlockRoot.BeginAnimation(OpacityProperty, fadeOutAnimation);
+        }
+
+        private static SolidColorBrush GetMessageLevelBrush(ShowActionMessage.LevelType level)
+        {
+            return level switch
+            {
+                ShowActionMessage.LevelType.Info => Brushes.Blue,
+                ShowActionMessage.LevelType.Warn => Brushes.Orange,
+                ShowActionMessage.LevelType.Error => Brushes.Red,
+                _ => Brushes.Black,
+            };
+        }
+
+        public readonly struct ShowActionMessage
+        {
+            public enum LevelType
+            {
+                Info,
+                Warn,
+                Error,
+            }
+
+            public readonly string Message;
+            public readonly LevelType Level;
+            public readonly TimeSpan FadeDuration;
+            public readonly TimeSpan VisibleDuration;
+
+            public ShowActionMessage(string message)
+            {
+                Message = message;
+                Level = LevelType.Info;
+                FadeDuration = DefaultFadeDuration;
+                VisibleDuration = DefaultVisibleDuration;
+            }
+
+            public ShowActionMessage(string message, LevelType level)
+            {
+                Message = message;
+                Level = level;
+                FadeDuration = DefaultFadeDuration;
+                VisibleDuration = DefaultVisibleDuration;
+            }
         }
     }
 }
